09e31eb6a3b439160abbb5e685d8717aac68ef81
Erik Fecher <efecher@gmail.com>
2021-11-11 14:30:00 -0500

    initial upload
    Concept for data entry for the Cost Calculator, for data maintenance, consider porting to typescript once the mechanics are down for consistency with the calculator itself

--- src/App.js
+++ src/App.js
@@ -1,25 +1,163 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [inputList, setInputList] = useState([{ 
+    gpaRangeLower: "", 
+    gpaRangeUpper: "", 
+    satRangeLower: "", 
+    satRangeUpper: "", 
+    actRangeLower: "", 
+    actRangeUpper: "", 
+    awardAmount: "" 
+  }]);
+ 
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+  
+  // handle click event of the Remove button
+  const handleRemoveClick = index => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+  
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { gpaRangeLower: "", gpaRangeUpper: "", satRangeLower: "", satRangeUpper: "", actRangeLower: "", actRangeUpper: "", awardAmount: "" }]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let p = document.createElement('p');
+    p.innerHTML = JSON.stringify(convertInputData(inputList));
+    document.body.appendChild(p);
+    return;
+  };
+
+  const convertInputData = (data) => {
+    //console.log(data);
+    let output = [];
+    console.log(output);
+    for(let i=0; i<data.length; i++) {
+      let row = [];
+
+      row.push(
+        // NOTE: Check if input is a number first before converting from string, empty strings, as in the initial values of the inputs are NaN
+        (data[i].gpaRangeLower === "")? 0 : parseFloat(data[i].gpaRangeLower), 
+        (data[i].gpaRangeUpper === "")? 0 : parseFloat(data[i].gpaRangeUpper), 
+        (data[i].satRangeLower === "")? 0 : parseInt(data[i].satRangeLower), 
+        (data[i].satRangeUpper === "")? 0 : parseInt(data[i].satRangeUpper), 
+        (data[i].actRangeLower === "")? 0 : parseInt(data[i].actRangeLower), 
+        (data[i].actRangeUpper === "")? 0 : parseInt(data[i].actRangeUpper), 
+        (data[i].awardAmount === "")? 0 : parseInt(data[i].awardAmount)
+      );
+      output.push(row);
+    }
+
+    return output;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <h3>Merit Based - Matrix With Test Scores</h3> 
+        <table>
+          <tr>
+            <th>GPA Range Lower Bound</th>
+            <th>GPA Range Upper Bound</th>
+            <th>SAT Range Lower Bound</th>
+            <th>SAT Range Upper Bound</th>
+            <th>ACT Range Lower Bound</th>
+            <th>ACT Range Upper Bound</th>
+            <th>Award Amount</th>
+            <th>&nbsp;</th>
+          </tr>
+          {inputList.map((x, i) => {
+            return (
+              <tr>
+                <td>
+                  <input
+                    name="gpaRangeLower"
+                    placeholder="0"
+                    value={x.gpaRangeLower}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                </td>
+                <td>
+                  <input
+                    className="ml10"
+                    name="gpaRangeUpper"
+                    placeholder="0"
+                    value={x.gpaRangeUpper}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                </td>
+                <td>
+                  <input
+                    name="satRangeLower"
+                    placeholder="0"
+                    value={x.satRangeLower}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                </td>
+                <td>
+                  <input
+                    className="ml10"
+                    name="satRangeUpper"
+                    placeholder="0"
+                    value={x.satRangeUpper}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                </td>
+                <td>
+                  <input
+                    name="actRangeLower"
+                    placeholder="0"
+                    value={x.actRangeLower}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                </td>
+                <td>
+                  <input
+                    className="ml10"
+                    name="actRangeUpper"
+                    placeholder="0"
+                    value={x.actRangeUpper}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                </td>
+                <td>
+                  <input
+                    className="ml10"
+                    name="awardAmount"
+                    placeholder="0"
+                    value={x.awardAmount}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                </td>
+              <td>
+                <div className="btn-box">
+                  {inputList.length !== 1 && <button
+                    className="remove-button"
+                    onClick={() => handleRemoveClick(i)}>Remove Row</button>}
+                  {inputList.length - 1 === i && <button className="add-button" onClick={handleAddClick}>Add Row</button>}
+                </div>
+              </td>
+            </tr>
+          );
+        })}
+        </table>
+        <div class="submit-button-row">
+          <input type="submit" value="Submit Matrix" />
+        </div>
+      </form>
+    </div> 
   );
 }
-
 export default App;
