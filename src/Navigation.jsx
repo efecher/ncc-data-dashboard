4dc0f80501d98c89094a8144c5786893353beaf6
Erik Fecher <efecher@gmail.com>
2022-01-31 16:40:59 -0500

    EOD commit
    save point

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import './style/Navigation.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Navigation(props) {
+  
+  const renderOptions = (config) => {
+    console.log(config[0]);
+    let options = [];
+
+    for (let i=0; i < config.length; i++) {
+      options.push(
+        <option value={i} key={uuidv4()}>{config[i].matrixName}</option>
+      );
+    }
+    return options;
+  };
+  
   return (
       <>
       <p>Select from the items below to begin editing data:</p>
       <select onChange={(e) => {props.handler(e)}}>
-        <option value="Home">Home</option>
-        <option value="Freshman Merit With Test">Freshman Merit With Test</option>
-        <option value="Freshman Merit Test Optional">Freshman Merit (Test Optional)</option>
-        <option value="Freshman Needs Based NJ Residents">Freshman Needs Based NJ Residents</option>
-        <option value="Freshman Needs Based Non-NJ Residents">Freshman Needs Based: Non-NJ Residents</option>
-        <option value="Transfer Needs Based NJ Residents">Transfer: Needs Based (NJ Residents)</option>
-        <option value="Transfer Needs Based Non NJ Residents">Transfer: Needs Based (Non NJ Residents)</option>
-        <option value="Transfer Merit-Based GPA">Transfer: Merit-Based (GPA)</option>
-        <option value="Pell">Pell</option>
-        <option value="Tuition Assistance Grant">Tuition Assistance Grant</option>
-        <option value="EFC Dependent">EFC - Dependent</option>
-        <option value="EFC Independent with no dependents">EFC - Independent with no Dependents</option>
-        <option value="EFC Independent with Dependents">EFC - Independent with Dependents</option>
-        <option value="SHU Tuition and Expenses">SHU Tuition and Expenses</option>
+        {renderOptions(props.config)}
       </select>
       </>
   );
